ede6f4ab33a629c77a2d437d6978b5baec43e820
Bunhouth <bunhouth99@gmail.com>
2020-08-31 18:45:36 +0900

    auto prfix channel

--- socketcluster-client.js
+++ socketcluster-client.js
@@ -1,5 +1,5 @@
 /**
- * SocketCluster JavaScript client v16.0.1
+ * SocketCluster JavaScript client v16.0.2
  */
  (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.socketClusterClient = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 (function (global){
@@ -500,6 +500,9 @@ AGClientSocket.prototype._changeToAuthenticatedState = function (signedAuthToken
   this.signedAuthToken = signedAuthToken;
   this.authToken = this._extractAuthTokenData(signedAuthToken);
 
+  if(this.authToken && this.authToken instanceof Object && this.authToken.prefix) {
+    this.channelPrefix = this.authToken.prefix;
+  }
   if (this.authState !== this.AUTHENTICATED) {
     let oldAuthState = this.authState;
     this.authState = this.AUTHENTICATED;
